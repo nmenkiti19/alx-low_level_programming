f02b52bed2952fb950cea23dcb123d86748e8f6b
nmenkiti19 <ngomenkiti@gmail.com>
2023-03-20 14:10:11 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Description of the program - uses printf to print out a sentence */
+
+/**
+ * main - Entry point
+ *
+ * Description: The function main is the entry point of the program
+ * Return: 0 is returned showing SUCCESS
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
