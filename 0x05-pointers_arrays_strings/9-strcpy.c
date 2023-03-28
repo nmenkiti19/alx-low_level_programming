4f1f157598ed22c4a0884c30a86b49c0de57a6af
nmenkiti19 <ngomenkiti@gmail.com>
2023-03-28 22:18:08 +0000

    Task 9a

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *tmp = dest;
+	int i;
 
-	while (*src)
-		*dest++ = *src++;
-	return (tmp);
+	i = 0;
+	while (src[i] != '\0')
+
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
