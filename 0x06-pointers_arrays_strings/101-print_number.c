9a12233590bc5c98010e5aca11a767847dec2f49
nmenkiti19 <ngomenkiti@gmail.com>
2023-03-30 01:50:41 +0000

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
+{
+	unsigned int a, b, i;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		a = n * -1;
+	}
+	else
+	{
+		a = n;
+	}
+
+	b = a;
+	i = 1;
+
+	while (b > 9)
+	{
+		b /= 10;
+		i *= 10;
+	}
+
+	for (; i >= 1; i /= 10)
+	{
+		_putchar(((a / i) % 10) + 48);
+	}
+}
