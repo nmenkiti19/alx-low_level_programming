05755fe918cae831b7c879e7d5ddf00752ea4578
nmenkiti19 <ngomenkiti@gmail.com>
2023-04-05 15:03:54 +0000

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: function parameter
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	return (0);
+}
