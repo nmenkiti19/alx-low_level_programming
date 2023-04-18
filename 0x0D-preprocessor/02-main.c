c56125076140625e4d81a5524b809ebf72ec90a5
nmenkiti19 <ngomenkiti@gmail.com>
2023-04-18 21:44:09 +0000

    fix 2main bettyppcs

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main: function where name of file compiled 
- * from is printed
- * Return - always returns 0
+ * main - prints name of file where compiled
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-	printf("The name of the file it was compiled from is %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
