01730754c870d3e93ff5e709944c32f471f34399
nmenkiti19 <ngomenkiti@gmail.com>
2023-04-19 20:14:47 +0000

    fix Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
  * print_name - prints a name
- * @*name - parameter pointer
- * @*f - function pointer parameter
+ * @name - parameter pointer
+ * @f - function pointer parameter
  */
 void print_name(char *name, void (*f)(char *))
 {
