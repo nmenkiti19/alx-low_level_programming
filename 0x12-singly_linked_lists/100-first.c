5210774e17f45c38f9587f77157eaa62c7d8b220
nmenkiti19 <ngomenkiti@gmail.com>
2023-04-29 00:20:13 +0000

    Task 5 and 6

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * toRunBeforeMain - runs before main() executes
+ */
+void toRunBeforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
