bc1ce63b2d4fd523a73de2d15d99c8a91f617451
nmenkiti19 <ngomenkiti@gmail.com>
2023-07-10 05:36:17 -0700

    fixed betty for task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	while (text_content && *(text_content + size))
+	while (text_content && *(text_content + size)
 			size++;
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
